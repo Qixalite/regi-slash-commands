933b6ef15074e4d180d6e6a8325768456035b22c
c43721 <rlahman.game@gmail.com>
2021-02-03 16:22:48 -0600

    Fix config.rs

--- src/discord/command/config.rs
+++ src/discord/command/config.rs
@@ -6,9 +6,7 @@ pub(crate) fn config() -> InteractionResponse {
     InteractionResponse {
         ty: InteractionResponseType::ChannelMessageWithSource,
         data: Some(InteractionApplicationCommandCallbackData {
-            content:
-                "https://docs.qixalite.com/guides/games/tf2/bk-configs.html#respawn-league-rsl"
-                    .to_string(),
+            content: "https://docs.qixalite.com/guides/games/tf2/bk-configs.html".to_string(),
         }),
     }
 }
